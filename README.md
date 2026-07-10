021ad54d451050100ad4688c3d2b994c484b7434
Les Aker <me@lesaker.org>
2026-07-09 20:16:32 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+puppet-policybot
+===========
+
+[![Puppet Forge](https://img.shields.io/puppetforge/v/halyard/policybot.svg)](https://forge.puppetlabs.com/halyard/policybot)
+[![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/halyard/puppet-policybot/build.yml?branch=main)](https://github.com/halyard/puppet-policybot/actions)
+[![MIT Licensed](http://img.shields.io/badge/license-MIT-green.svg?style=flat)](https://tldrlegal.com/license/mit-license)
+
+Module to configure [policy-boy](https://github.com/palantir/policy-bot) app for HomeAssistant
+
+## Usage
+
+```puppet
+include policybot
+```
+## License
+
+policybot is released under the MIT License. See the bundled LICENSE file for details.
+
---
+++ manifests/init.pp
@@ -0,0 +1,40 @@
+# @summary Configure Policy-bot container
+#
+# @param hostname is the hostname
+# @param aws_access_key_id sets the AWS key to use for Route53 challenge
+# @param aws_secret_access_key sets the AWS secret key to use for the Route53 challenge
+# @param email sets the contact address for the certificate
+# @param github_integration_id sets the integration ID for the GitHub app
+# @param github_webhook_secret sets the webook secret for validating webhooks
+# @param github_private_key sets the private key for the GitHub app
+# @param github_client_id sets the Client ID for OAuth
+# @param github_client_secret sets the Client Secret for OAuth
+# @param session_key sets the cookie encryption key
+# @param container_ip sets the IP address for the docker container
+# @param port sets the port to listen on
+class policybot (
+  String $hostname,
+  String $aws_access_key_id,
+  String $aws_secret_access_key,
+  String $email,
+  String $github_integration_id,
+  String $github_webhook_secret,
+  String $github_private_key,
+  String $github_client_id,
+  String $github_client_secret,
+  String $session_key,
+  String $container_ip = '172.17.0.2',
+) {
+  docker::container { 'policybot':
+    image => 'palantirtechnologies/policy-bot:latest',
+    args  => [],
+    cmd   => '',
+  }
+
+  nginx::site { $hostname:
+    proxy_target          => "http://${container_ip}:8091",
+    aws_access_key_id     => $aws_access_key_id,
+    aws_secret_access_key => $aws_secret_access_key,
+    email                 => $email,
+  }
+}
